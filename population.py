3914731a5444ed7b904f6947a3668fd5d1ee5854
harishkumar1218 <89627244+harishkumar1218@users.noreply.github.com>
2021-11-12 17:42:35 +0530

    Update population.py

--- population.py
+++ population.py
@@ -17,37 +17,27 @@ class pop:
       def fac(self,p,j):
           i=j
           d=1
-
           while(i>=0):
               d*=p-i
               i-=1
           return d
 
       def predict(self,r):
-          #d=min(x[::len(x)-1], key=lambda x:abs(x - r))
           d=x[0]
           h = y[x.index(d)]
           z = (r - d) / (x[1] - x[0])
           l=self.lis(y)
           l.reverse()
-          #if (r < d):
-           #   for k in l:
-           #       k.reverse()
-            #      print(k)
           o=len(y)-1
-
           for i in l:
               for j in i:
                   h += self.fac(z,o-1) * j / (mt.factorial(o))
                   break
               o = o - 1
           return h
-
-
-
 p=pop()
-y=[2.8155,2.8182,2.8208,2.8234]
-x=[654,658,662,666]
+x=[1891,1901,1911,1921,1931]
+y=[46,66,81,93,101]
 print(p.predict(670))
 plt.xlabel("x axis")
 plt.ylabel("y axis")
